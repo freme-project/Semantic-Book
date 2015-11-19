1337be876ba472be1151baea2836a6b235196165
Ben De Meester <ben.demeester@ugent.be>
2015-11-19 10:37:37 +0100

    some typos

--- step-4/e-link.sh
+++ step-4/e-link.sh
@@ -5,10 +5,10 @@ fi
 
 if [ ! -e "./nif" ]
 then
-	mkdir ./out
+	mkdir ./nif
 fi
 
-for file in administration cityscape culture_and_comtemporary_life demographics economy education environment etymology external_links geography history international_relations introduction elympic_games other_locations_named_after_athens references see_also special_olympics transport
+for file in administration cityscape culture_and_contemporary_life demographics economy education environment etymology external_links geography history international_relations introduction olympic_games other_locations_named_after_athens references see_also special_olympics transport
 do	
 	curl -X POST --header "Content-Type: application/ld+json" --header "Accept: text/n3" --header "X-Auth-Token: $1" -d "@../step-3/nif/$file.html" "http://api-dev.freme-project.eu/current/e-link/documents/?templateid=4452" > ./nif/$file.ttl
 	cp ../step-3/out/$file.html ./out
