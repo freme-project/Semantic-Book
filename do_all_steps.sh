0112a1aa0a16063bc6f1319c35486be5a52a858a
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-01-05 09:35:23 +0100

    removed xhtml hack

--- do_all_steps.sh
+++ do_all_steps.sh
@@ -29,10 +29,10 @@ echo 'Step 8...'
 ./createEPUB.sh &> log.log
 
 #hack for search.xhtml
-cd ./out;
-unzip semantic_book.epub;
-rm -f semantic_book.epub;
-cp ../../step-7/search.xhtml OEBPS;
-zip -X semantic_book.epub mimetype;
-zip -rg semantic_book.epub META-INF -x \*.DS_Store;
-zip -rg semantic_book.epub OEBPS -x \*.DS_Store;
+#cd ./out;
+#unzip semantic_book.epub;
+#rm -f semantic_book.epub;
+#cp ../../step-7/search.xhtml OEBPS;
+#zip -X semantic_book.epub mimetype;
+#zip -rg semantic_book.epub META-INF -x \*.DS_Store;
+#zip -rg semantic_book.epub OEBPS -x \*.DS_Store;
